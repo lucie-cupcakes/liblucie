ea6f188e2a7d4aa016b2cf17540a34a84ce52d04
Lucie Cupcakes <lucie_linux@protonmail.com>
2021-04-27 13:47:05 -0300

    Updated copyright headers

--- memcpy.c
+++ memcpy.c
@@ -1,13 +1,5 @@
-/*
- * tb_memcpy.c
- *
- * Copyright (c) 2021 Luis "Laffin" Espinoza <laffintoo@gmail.com>
- * All rights reserved.
- *
- * Licensed under BSD-3-Clause  - A copy of the license can be found in file
- * LICENSE included with the source package or you can view the full text at
- * https://spdx.org/licenses/BSD-3-Clause
- */
+// Copyright (c) 2021 Lucie Cupcakes <lucie_linux@protonmail.com>
+// Licensed under BSD-3-Clause - https://spdx.org/licenses/BSD-3-Clause
 
 #include "strings.h"
 
